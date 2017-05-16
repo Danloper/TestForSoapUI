e7df574e08e9883fc06549fbbe7b849b8bd26cf1
D070033 <D070033@XXPN32384761A.emea.global.corp.sap>
2017-05-16 09:37:21 +0200

    updated

---
+++ src/main/java/de/sap/daniel/odata/test/App.java
@@ -0,0 +1,13 @@
+package de.sap.daniel.odata.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
